80ae22851545d1f0444ee58537c020d2041d4a09
Maksym Korniiko <korniykom@gmail.com>
2024-07-01 11:22:32 +0300

    fead: add limit to max amount of clicks

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,29 +1,63 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const MAX_ALLOWED_CLICKS = 2;
+
+  const [currentClicks, setCurrentClicks] = useState(0);
+  const [totalClicks, setTotalClicks] = useState(0);
+
+  function handleDecrease() {
+    setCurrentClicks((clicks) => {
+      if (clicks <= -MAX_ALLOWED_CLICKS) return clicks;
+
+      return clicks - 1;
+    });
+    setTotalClicks((clicks) => clicks + 1);
+  }
+
+  function handleReset() {
+    setCurrentClicks((clicks) => 0);
+    setTotalClicks((clicks) => clicks + 1);
+  }
+
+  function handleIncrease() {
+    setCurrentClicks((clicks) => {
+      if (clicks >= MAX_ALLOWED_CLICKS) return clicks;
+
+      return clicks + 1;
+    });
+    setTotalClicks((clicks) => clicks + 1);
+  }
+
   return (
     <div>
-      <Header />
-      <CountCard />
+      <Header totalClicks={totalClicks} />
+      <CountCard
+        currentClicks={currentClicks}
+        onDeacrease={handleDecrease}
+        onReset={handleReset}
+        onIncrease={handleIncrease}
+      />
       <Footer />
     </div>
   );
 }
 
-function CountCard() {
+function CountCard({ currentClicks, onDeacrease, onReset, onIncrease }) {
   return (
     <div className="container">
       <div className="count-card">
         <span className="title">Counter App</span>
-        <span className="number">0</span>
+        <span className="number">{currentClicks}</span>
         <div>
-          <Button color="#222" backgroundColor="#FF605C">
+          <Button color="#222" backgroundColor="#FF605C" onClick={onDeacrease}>
             Decrease{" "}
           </Button>
-          <Button color="#222" backgroundColor="#ffd43b">
+          <Button color="#222" backgroundColor="#ffd43b" onClick={onReset}>
             Reset
           </Button>
-          <Button color="#222" backgroundColor="#00CA4E">
+          <Button color="#222" backgroundColor="#00CA4E" onClick={onIncrease}>
             Increase
           </Button>
         </div>
@@ -33,24 +67,25 @@ function CountCard() {
   );
 }
 
-function Header() {
+function Header({ totalClicks }) {
   return (
     <header>
       <div className="total-clicks">
-        <span>Total clicks: X</span>
+        <span>Total clicks: {totalClicks}</span>
       </div>
     </header>
   );
 }
 
-function Footer() {
-  return <footer>Count 'em all!</footer>;
+function Footer({ children }) {
+  return <footer>{children}</footer>;
 }
 
 function Button({
   color = "#fff",
   backgroundColor = "#38f",
   children = "Button",
+  onClick,
 }) {
   const buttonStyle = {
     color,
@@ -58,7 +93,7 @@ function Button({
     boxShadow: `6px 6px 5px 2px ${"#212529"}`,
   };
   return (
-    <button style={buttonStyle} className="action-button">
+    <button onClick={onClick} style={buttonStyle} className="action-button">
       {children}
     </button>
   );
